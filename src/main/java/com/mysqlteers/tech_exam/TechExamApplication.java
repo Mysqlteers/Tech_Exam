e07ae504d1387bd4f731df06bd5c8fef8edb2ad6
DanielMejlvang <58211711+caerKEA@users.noreply.github.com>
2020-12-11 10:10:11 +0100

    Test push

--- src/main/java/com/mysqlteers/tech_exam/TechExamApplication.java
+++ src/main/java/com/mysqlteers/tech_exam/TechExamApplication.java
@@ -6,10 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TechExamApplication {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         SpringApplication.run(TechExamApplication.class, args);
-        System.out.println("Spring is up and running");
     }
 
 }
